0cc3b2e5656f0f5dc5b39beda6cb42cd6b688e01
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2023-01-13 14:09:21 -0300

    adding log info

--- app/src/main/java/com/dev/myappcompany/currencyconverter/MainActivity.java
+++ app/src/main/java/com/dev/myappcompany/currencyconverter/MainActivity.java
@@ -3,8 +3,24 @@ package com.dev.myappcompany.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertButton(View view) {
+
+        Log.i("Info", "Button Pressed");
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        Log.i("Info", editText.getText().toString());
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
